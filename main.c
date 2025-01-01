2261ad72ba78a5045617c38cf285da1453346917
akram khan <akramkhan@akrams-MacBook-Air.local>
2025-01-01 16:02:28 +0530

    added file system for record and improved visibility

--- main.c
+++ main.c
@@ -8,40 +8,48 @@ typedef struct employee
     int experience;
     char joiningDate[11];
 } employee;
-void create()
+void create(FILE *fp)
 {
     employee e;
     printf("Enter the employeeID: ");
     scanf("%d", &e.employeeId);
     printf("Enter the Employee Name: ");
-    scanf("%s", &e.name);
+    scanf("%s", e.name);
     printf("Enter the age: ");
     scanf("%d", &e.age);
     printf("Enter the Designation: ");
-    scanf("%s", &e.designation);
+    scanf("%s", e.designation);
     printf("Enter the experience: ");
     scanf("%d", &e.experience);
     printf("Enter the Joining Date: ");
-    scanf("%s", &e.joiningDate);
+    scanf("%s", e.joiningDate);
+    if(fp == NULL){
+        printf("\nERROR file not opened");
+    }
+    else{
+        fprintf(fp,"employeeID: %d, Employee Name: %s, age: %d, Designation: %s, experience: %d, Joining Date: %s\n", e.employeeId,e.name,e.age,e.designation,e.experience,e.joiningDate);
+    }
 }
 int main()
 {
+    FILE *fp;
     int c;
+    fp = fopen("record.txt","a+");
+    printf("\n\n\n\n---------------- WELCOME TO EMPLOYEE MANAGEMENT SYSTEM ----------------\n\n");
     do
     {
-        printf("Welcome to Employee Managment System");
-        printf("\nSelect the below option");
-        printf("\n1.Add a new Entry");
-        printf("\n2.Delete entries");
-        printf("\n3.Search a Record");
-        printf("\n4.Sort by age");
-        printf("\n5.Display Record");
-        printf("\n6.Exit\n");
+        printf("\nSelect the below option\n");
+        printf("\n\t1. Add a new Entry");
+        printf("\n\t2. Delete entries");
+        printf("\n\t3. Search a Record");
+        printf("\n\t4. Sort by age");
+        printf("\n\t5. Display Record");
+        printf("\n\t6. Exit\n");
         scanf("%d", &c);
         switch (c)
         {
         case 1:
-            create();
+            create(fp);
             break;
         case 2:
 
